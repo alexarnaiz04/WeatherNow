a971c23e4809e2a6a28a2b7476fb4149049a0a71
alexarnaiz04 <115986338+alexarnaiz04@users.noreply.github.com>
2026-05-27 21:45:56 +0200

    aa

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 }
 
 val openWeatherApiKey = project.findProperty("OPEN_WEATHER_API_KEY") as? String ?: ""
+
 android {
     namespace = "com.example.weathernow"
     compileSdk = 35
@@ -17,7 +18,8 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        buildConfigField("String", "OWM_API_KEY", "\"$openWeatherApiKey\"")    }
+        buildConfigField("String", "OWM_API_KEY", "\"$openWeatherApiKey\"")
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -35,10 +37,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation("androidx.activity:activity-compose:1.10.1")
 
     implementation(platform("androidx.compose:compose-bom:2024.10.01"))
@@ -46,8 +45,13 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
 
     implementation("androidx.navigation:navigation-compose:2.8.5")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.9.0")
 
@@ -55,13 +59,15 @@ dependencies {
     implementation("androidx.room:room-ktx:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
 
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+
     implementation("io.coil-kt:coil-compose:2.7.0")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
-    implementation("com.google.android.gms:play-services-location:21.3.0")
-    implementation("androidx.datastore:datastore-preferences:1.1.1")
 }
--- app/src/main/java/com/example/weathernow/data/local/WeatherDao.kt
+++ app/src/main/java/com/example/weathernow/data/local/WeatherDao.kt
@@ -1,7 +1,6 @@
 package com.example.weathernow.data.local
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
 import kotlinx.coroutines.flow.Flow
