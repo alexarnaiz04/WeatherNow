38557aa399ee3e1e3ca6b09e6148b6bfac224a16
alexarnaiz04 <115986338+alexarnaiz04@users.noreply.github.com>
2026-05-27 21:40:41 +0200

    aa

--- app/src/main/java/com/example/weathernow/domain/model/ForecastItem.kt
+++
@@ -1,8 +0,0 @@
-package com.example.weathernow.domain.model
-
-data class ForecastItem(
-    val dateTimeEpoch: Long,
-    val temperatureCelsius: Double,
-    val conditionText: String,
-    val iconCode: String
-)
--- app/src/main/java/com/example/weathernow/domain/model/Location.kt
+++
@@ -1,8 +0,0 @@
-package com.example.weathernow.domain.model
-
-data class Location(
-    val cityName: String,
-    val countryCode: String,
-    val latitude: Double,
-    val longitude: Double
-)
